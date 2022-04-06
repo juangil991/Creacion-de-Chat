e28e23f1c40b8d884abce9f487cdce924f7e7eab
juangil991 <jcgo125@gmail.com>
2022-04-06 12:23:27 -0500

    se añade la configurarion de firebase y  router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,15 @@
-
+import React, { Component } from 'react';
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import Home from './pages/Home';
+import Chat from './pages/Chat';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import { auth } from './services/firebase';
 
 function App() {
   return (
